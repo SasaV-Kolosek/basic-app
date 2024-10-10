a19a643014579536984545533e2d78d744e94a1d
SasaV-Kolosek <sasavas@kolosek.com>
2024-10-10 13:21:43 +0200

    failed spec

--- spec/routing/items_routing_spec.rb
+++ spec/routing/items_routing_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.describe ItemsController, type: :routing do
   describe "routing" do
     it "routes to #index" do
-      expect(get: "/items").to route_to("items#index")
+      expect(get: "/items").not_to route_to("items#index")
     end
 
     it "routes to #new" do
