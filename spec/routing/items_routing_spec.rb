6a1ea8dd68bb060fc705bc7cfdff922e0718b21c
SasaV-Kolosek <sasavas@kolosek.com>
2024-10-10 13:25:39 +0200

    another failed specs

--- spec/routing/items_routing_spec.rb
+++ spec/routing/items_routing_spec.rb
@@ -3,6 +3,10 @@ require "rails_helper"
 RSpec.describe ItemsController, type: :routing do
   describe "routing" do
     it "routes to #index" do
+      expect(true).to eq(false)
+    end
+
+    it "routes to #index" do
       expect(get: "/items").not_to route_to("items#index")
     end
 
@@ -11,7 +15,7 @@ RSpec.describe ItemsController, type: :routing do
     end
 
     it "routes to #show" do
-      expect(get: "/items/1").to route_to("items#show", id: "1")
+      expect(get: "/items/1").to route_to("items#show", id: "4")
     end
 
     it "routes to #edit" do
